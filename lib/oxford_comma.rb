ae54ec3d36cc91dd02a7ceb54b5d73f400c8fac6
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 20:19:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ if array.length == 1
   elsif array.length == 2 
   array[0] + " and " + array[1]
 else 
-  array[-1] = "and" + array[-1]
-  array.join(",")
+  array[-1] = "and " + array[-1]
+  array.join(", ")
  end
 end 
